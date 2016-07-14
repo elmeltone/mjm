418f1bca9d8f5d5846cc205ba67488aa58a30ac8
MICHAEL J MELTON <meltonm83@gmail.com>
2016-07-14 08:32:02 -0700

    Adds html shell and sass, cleans up server

--- server.js
+++ server.js
@@ -6,21 +6,6 @@ App.get('/', function(request, response) {
   response.send('This site is under construction. Please come back later :)');
 });
 
-App.get('/headers', function(request, response) {
-  var echo = request.headers;
-  response.send(echo).json;
-});
-
-App.get('/headers/:header_name', function(request, response) {
-  var headerName = request.headers[request.params.header_name];
-  response.send(headerName);
-});
-
-App.get('/version', function(request, response) {
-  var version = request.httpVersion;
-  response.send(version);
-});
-
 App.listen((process.env.PORT || 8080));
 
 exports.App = App;
