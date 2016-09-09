cd86c13a474788f5f01dd7196b853c10d99602f0
MICHAEL J MELTON <meltonm83@gmail.com>
2016-09-09 15:35:05 -0700

    Reverts server to bare minimum

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
-var express = require('express');
+const express = require('express');
+const app = express();
 
-var App = express();
+app.use(express.static(__dirname + '/'));
 
-App.use(express.static(__dirname));
-
-App.get('*', function(req, res){
-  res.sendfile(__dirname + 'index.html');
-});
-
-App.listen((process.env.PORT || 8080));
-
-exports.App = App;
+app.listen(process.env.PORT || 8080);
