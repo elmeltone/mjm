80c6b8e7ba3ec1ca2830a2b4c0eaa45821405802
MICHAEL J MELTON <meltonm83@gmail.com>
2016-11-08 10:17:05 -0800

    Formats .follow, changes hamburger on hover, fixes footer

--- index.js
+++ index.js
@@ -3,4 +3,11 @@ $(function() {
     $('.line-wrapper').on('click', function() {
         $('.options-wrapper').toggleClass('icon-unclicked icon-clicked');
     });
+  //Changes hamburger icon background color on hover
+    $('.line-wrapper').on('mouseenter', function() {
+        $('.line').toggleClass('dark light');
+    });
+    $('.line-wrapper').on('mouseleave', function() {
+        $('.line').toggleClass('dark light');
+    });
 });
